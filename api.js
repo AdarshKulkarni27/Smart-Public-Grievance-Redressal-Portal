9824bc39d885b80e1653d74cbfd38d0d04db74ba
Adarsh Kulkarni <adarshkulkarni27@gmail.com>
2026-06-16 11:47:37 +0530

    Delete api.js

--- api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  // THIS IS THE MAGIC LINE. It must point to your Flask server's /api folder
-  baseURL: 'http://localhost:5000/api', 
-});
-
-export default api;
